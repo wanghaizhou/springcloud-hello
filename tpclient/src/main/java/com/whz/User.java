bf2c630351d2d358b275ab5fd6d2a6c5c95e7e0d
王阿牛 <1075444875@qq.com>
2018-08-22 15:32:57 +0800

    swagger2

---
+++ tpclient/src/main/java/com/whz/User.java
@@ -0,0 +1,17 @@
+package com.whz;
+
+/**
+ * Created by wanghaizhou on 2018/8/22.
+ */
+public class User {
+
+    private Integer baseId;
+
+    public Integer getBaseId() {
+        return baseId;
+    }
+
+    public void setBaseId(Integer baseId) {
+        this.baseId = baseId;
+    }
+}
